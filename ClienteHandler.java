46304c7eed8eca0c2ea34fea5ef3459c5dd28bdf
Mika <kakamarques.0202@gmail.com>
2025-06-26 01:27:09 -0300

    atualizacao classe

--- ClienteHandler.java
+++ ClienteHandler.java
@@ -1,5 +1,24 @@
 package ServerChat;
-
-public class ClienteHandler {
-
+import java.io.*;
+import java.net.*;
+public class ClienteHandler extends Thread {
+	private Socket socket;
+	private BufferedReader entrada;
+	private PrintWriter saida;
+	private String nomeUsuario;
+	private boolean ehAdmin;
+	private SalaChat salaAtual;
+	private ServidorChat servidor;
+	
+	public ClienteHandler(Socket socket, ServidorChat servidor) {
+		this.socket = socket;
+		this.servidor = servidor;
+		
+		try {
+			entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			saida = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			System.err.println("Erro ao criar streams para o usuário: " + e.getMessage());
+		}
+	}
 }
